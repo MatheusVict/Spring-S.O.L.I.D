a90f74a6685494a2831371a5dfd75795ae03d3d0
MatheusVict <matheusvictorhenrique@gmail.com>
2023-05-25 13:03:32 -0300

    service interfaces

---
+++ src/main/kotlin/com/learning/solid/creditapplication/services/ICostumerService.kt
@@ -0,0 +1,10 @@
+package com.learning.solid.creditapplication.services
+
+import com.learning.solid.creditapplication.entity.Customer
+
+interface ICostumerService {
+
+    fun save(customer: Customer): Customer
+    fun findById(customerId: Long): Customer
+    fun delete(customerId: Long): Void
+}
---
+++ src/main/kotlin/com/learning/solid/creditapplication/services/ICreditService.kt
@@ -0,0 +1,12 @@
+package com.learning.solid.creditapplication.services
+
+import com.learning.solid.creditapplication.entity.Credit
+import org.aspectj.apache.bcel.classfile.Code
+import java.util.UUID
+
+interface ICreditService {
+
+    fun save(credit: Credit): Credit
+    fun findAllByCostumer(costumerId: Long): List<Credit>
+    fun findByCreditCode(creditCode: UUID): Credit
+}
